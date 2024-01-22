f51aefe38e18396af3ac0a0a9244d603fd8aef0e
jaesalaski <jae@salaski.net>
2024-01-22 15:41:42 -0600

    Feedback Data for State

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import {useState} from 'react'
+import FeedbackData from "./data/FeedbackData";
 
 export default function App(){
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
       <Header text="Sample School" />
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData =
+    [
+        {
+        id: 1,
+        rating: 10, 
+        text: 'Lorem ipsum dolor',
+        },
+        {
+        id: 2,
+        rating: 5, 
+        text: 'Molar simpu rolod',
+        }
+        {
+        id: 3,
+        rating: 2, 
+        text: 'isbad',
+        }
+    ]
+
+    export default FeedbackData
