48eb104b844540ea85402697ef2d2cb4eff22656
jaesalaski <jae@salaski.net>
2024-02-01 09:42:01 -0600

    saving work

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,14 @@
 import Card from "./shared/Card"
+import {FaTimes} from 'react-icons/fa'
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+
     return (
         <Card reverse={true}>
             <div className='num-display'>{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='red' />
+            </button>
             <div className='text-display'>{item.text}</div>
         </Card>
     )
